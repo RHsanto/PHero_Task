ed4db864bbc9c69b9981d9dc205d38e8c5de62ed
RHsanto <rakibulsanto789@gmail.com>
2022-06-30 16:04:42 +0600

    add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,16 +9,16 @@ const Home = () => {
   <div className="navbar ">
     <div className="container ">
       <div><h2>Power Hack</h2></div>
-      <div><p>Paid Total :0</p></div>
+      <div><p>Paid Total : 0</p></div>
     </div>
   </div>
-   {/* search section */}
 
-   <div className="container ">
-    <div className='searchSection d-flex align-items-center  justify-content-between'>
-    <div className="searchField">
-      <p>Billings</p>
-      <input type="search" name="" id="" />
+{/* search section */}
+  <div className="container">
+    <div className='searchSection mt-5 d-flex align-items-center  justify-content-between'>
+    <div className="searchField d-flex">
+      <p className='me-4'>Billings</p>
+      <input type="search" name="" id="" placeholder='Search'/>
     </div>
     <div className="AddBtn">
     <button>Add New Bill</button>
