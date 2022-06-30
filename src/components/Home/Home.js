39929c9533042a188bd225714c0778cc099f7e7b
RHsanto <rakibulsanto789@gmail.com>
2022-06-30 15:52:55 +0600

    add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Billing from './Billing/Billing';
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home
-Home</h1>
+    <div className='home'>
+  {/*Navbar  */}
+  <div className="navbar ">
+    <div className="container ">
+      <div><h2>Power Hack</h2></div>
+      <div><p>Paid Total :0</p></div>
+    </div>
+  </div>
+   {/* search section */}
+
+   <div className="container ">
+    <div className='searchSection d-flex align-items-center  justify-content-between'>
+    <div className="searchField">
+      <p>Billings</p>
+      <input type="search" name="" id="" />
+    </div>
+    <div className="AddBtn">
+    <button>Add New Bill</button>
+    </div>
+    </div>
+   </div>
+
+    <Billing/>
+    
     </div>
   );
 };
---
+++ src/components/Home/Submission/SubForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubForm = () => {
+  return (
+    <div>
+      <h1>SubForm
+SubForm</h1>
+    </div>
+  );
+};
+
+export default SubForm;
