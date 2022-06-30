b96db4ce7c96a24d5042e8d18510f3f543e80b95
RHsanto <rakibulsanto789@gmail.com>
2022-06-30 21:59:40 +0600

    add

--- src/components/Home/Submission/SubForm.js
+++ src/components/Home/Submission/SubForm.js
@@ -1,6 +1,22 @@
+import axios from 'axios';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const SubForm = () => {
+  const { register, handleSubmit ,reset} = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    axios.post('http://localhost:8000/add-billing',data)
+    .then(res =>{
+      console.log(res);
+      if(res.data.insertedId){
+        alert('Radio Station Add Successfully');
+        window.location.reload()
+
+      }
+    })
+  
+};
   
   return (
     <div>
@@ -10,6 +26,7 @@ const SubForm = () => {
 </button>
 
 {/* <!-- Modal --> */}
+<form onSubmit={handleSubmit(onSubmit)}>
 <div class="modal fade text-dark" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -18,33 +35,37 @@ const SubForm = () => {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 {/*Start  Model Body*/}
-     <div class="modal-body">
+<div class="modal-body">
       <div class="form-floating mb-3">
-        <input type="text" class="form-control" id="floatingInput" placeholder="rakibul santo"/>
+        <input type="text" {...register("name", { required: true })} class="form-control" id="floatingInput" placeholder="rakibul santo"/>
         <label for="floatingInput">Enter Your Name</label>
      </div>
       <div class="form-floating mb-3">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+        <input type="email" {...register("email", { required: true })} class="form-control" id="floatingInput" placeholder="name@example.com"/>
         <label for="floatingInput">Enter Email address</label>
      </div>
       <div class="form-floating mb-3">
-        <input type="tel" class="form-control" id="floatingInput" placeholder="Mobile"/>
+        <input type="tel" {...register("phone", { required: true })} class="form-control" id="floatingInput" placeholder="Mobile"/>
         <label for="floatingInput">Enter Phone Number</label>
      </div>
       <div class="form-floating mb-3">
-        <input type="number" class="form-control" id="floatingInput" placeholder="Mobile"/>
+        <input type="number" {...register("amount", { required: true })} class="form-control" id="floatingInput" placeholder="Mobile"/>
         <label for="floatingInput">Enter Amount </label>
      </div>
   
       </div>
+     
   {/*Start  Model Footer*/}
       <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <button data-bs-dismiss="modal" className= ' btn btn-success px-5 py-2' type="submit" >Submit</button>
       </div>
     </div>
   </div>
+
 </div>
+</form>
+
+
     </div>
   );
 };
